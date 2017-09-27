01205d4f833d1a479198b1b4f5809366fa761ebe
Ubuntu <ubuntu@ip-172-31-4-156.us-west-2.compute.internal>
2017-09-27 16:17:54 +0000

    route at start

--- route.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo iptables -t nat -D PREROUTING 1
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo iptables -t nat -D PREROUTING 1
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
 nohup node server.js &> vicimca.log &
